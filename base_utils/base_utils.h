61c8474989ee930caa900ec949f91bda67a52aba
wslsqo0e <shenming23@126.com>
2021-01-21 14:09:06 +0800

    update comment

--- base_utils/base_utils.h
+++ base_utils/base_utils.h
@@ -2,8 +2,6 @@
 > File Name:  base_utils.h
 > Author: shenming
 > Created Time: Tue Oct 20 18:46:13 2020
-
-  定义一些常用的函数，暂时放置在Logger模块中
 ************************************************************************/
 
 #ifndef __BASE_UTILS_H__
